b3ecf5398b7432b1a5ee8114f7a56042e7fcdc3d
Valentin Izmestev <v.n.izmestev@mail.ru>
2022-10-21 12:48:18 +0300

    Добавил поле загрузки файлов методом перетаскивания и застилизовал его

--- src/local/templates/lka/js/script.js
+++ src/local/templates/lka/js/script.js
@@ -2,6 +2,7 @@
 @@include('flatpickr.js');
 @@include('choices.min.js');
 @@include('jquery.maskedinput.js');
+@@include('jquery.filer.min.js');
 
 $(function(){
     let $eventBtn = $('.events');
@@ -68,6 +69,119 @@ $(function(){
         // $("#date").mask("99/99/9999");
         $(".tel").mask("+7 (999) 999-99-99"); 
         $(".snils").mask("999-999-999 99");  
+
+
+
+        $('.file-input').filer({
+            limit: null,
+            maxSize: null,
+            extensions: null,
+            changeInput: '<div class="jFiler-input-dragDrop"><div class="jFiler-input-inner"><div class="jFiler-input-icon"><i class="icon-jfi-cloud-up-o"></i></div><div class="jFiler-input-text"><h3>Перетащите файл сюда или кликните по полю</h3> <span>Выберите файл с расширением (jpg, jpeg, png, pdf) и размером не превышающим 1мб</span></div></div></div>',
+            showThumbs: true,
+            appendTo: null,
+            theme: "dragdropbox",
+            templates: {
+                box: '<ul class="jFiler-item-list"></ul>',
+                item: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <li>{{fi-progressBar}}</li>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+                itemAppend: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <span class="jFiler-item-others">{{fi-icon}} {{fi-size2}}</span>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+                progressBar: '<div class="bar"></div>',
+                itemAppendToEnd: false,
+                removeConfirmation: false,
+                _selectors: {
+                    list: '.jFiler-item-list',
+                    item: '.jFiler-item',
+                    progressBar: '.bar',
+                    remove: '.jFiler-item-trash-action',
+                }
+            },
+            uploadFile: {
+                url: "upload.php",
+                data: {},
+                type: 'POST',
+                enctype: 'multipart/form-data',
+                beforeSend: function(){},
+                success: function(data, el){
+                    var parent = el.find(".jFiler-jProgressBar").parent();
+                    el.find(".jFiler-jProgressBar").fadeOut("slow", function(){
+                        $("<div class=\"jFiler-item-others text-success\"><i class=\"icon-jfi-check-circle\"></i> Success</div>").hide().appendTo(parent).fadeIn("slow");    
+                    });
+                },
+                error: function(el){
+                    var parent = el.find(".jFiler-jProgressBar").parent();
+                    el.find(".jFiler-jProgressBar").fadeOut("slow", function(){
+                        $("<div class=\"jFiler-item-others text-error\"><i class=\"icon-jfi-minus-circle\"></i> Error</div>").hide().appendTo(parent).fadeIn("slow");    
+                    });
+                },
+                statusCode: {},
+                onProgress: function(){},
+            },
+            dragDrop: {
+                dragEnter: function(){},
+                dragLeave: function(){},
+                drop: function(){},
+            },
+            addMore: true,
+            clipBoardPaste: true,
+            excludeName: null,
+            beforeShow: function(){return true},
+            onSelect: function(){},
+            afterShow: function(){},
+            onRemove: function(){},
+            onEmpty: function(){},
+            captions: {
+                button: "Выберите файлы",
+                feedback: "Choose files To Upload",
+                feedback2: "Выберите файлы для Загрузки",
+                drop: "Поместите файл сюда для загрузки",
+                removeConfirmation: "Вы уверены, что хотите удалить этот файл?",
+                errors: {
+                    filesLimit: "Разрешается загружать только файлы {{fi-limit}}.",
+                    filesType: "К загрузке допускаются только изображения.",
+                    filesSize: "{{fi-name}} слишком большой! Пожалуйста, загрузите файл размером до {{fi-maxSize}} Мб.",
+                    filesSizeAll: "Выбранные вами файлы слишком велики! Пожалуйста, загружайте файлы размером до {{fi-maxSize}} Мб."
+                }
+            }
+        });
 });
 
 let greatShadow = document.querySelector('.great-shadow');
@@ -195,15 +309,19 @@ function fadeOut(elem) {
 	}, 1); 
 }
 
+// let testEl = document.querySelectorAll('.form-elem__input--validation');
+// console.log(testEl)
+
 /**Функция проверки на заполнненость полей страницы таба
  * @param {object} elem - елемент input 
  * возвращает массив с id контейнера и статусом его заполненности.
  */
 function checkingFullnessContainerFields(elem){ 
     let container = elem.closest('.form-questionnaire__item');
-    let containerInputs = container.querySelectorAll('.form-elem__input');
+    let containerInputs = container.querySelectorAll('.form-elem__input--validation');
     let fullesFlag = true; 
     containerInputs.forEach(input=>{
+        console.log(input + ' - ' + input.name + ' - ' + input.value);
         if(!input.value){
             fullesFlag = false;
         } 
